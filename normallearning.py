b714b159ca2ad871c421af68fda97a15cd7718a7
Pranavmath <pranav.sambhu@gmail.com>
2025-02-06 11:05:48 -0500

    did some stuff

--- normallearning.py
+++ normallearning.py
@@ -8,9 +8,10 @@ import torchvision
 from torchvision import transforms
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor, fasterrcnn_resnet50_fpn_v2 as fcnn
 import torch
-from pytorchref.coco_eval import CocoEvaluator
-from pytorchref.coco_utils import get_coco_api_from_dataset, _get_iou_types
+#from pytorchref.coco_eval import CocoEvaluator
+#from pytorchref.coco_utils import get_coco_api_from_dataset, _get_iou_types
 import wandb
+from torchmetrics.detection import MeanAveragePrecision
 
 wandb.init(project="diff model training")
 
@@ -51,10 +52,10 @@ cpu_device = torch.device("cpu")
 
 model = get_model().to(device)
 
-train_dataset = NoduleDataset("./refineddataset/trainxrays", "./refineddataset/control", "./refineddataset/nodules.json", 0.2, transform)
+train_dataset = NoduleDataset("./refineddataset/trainxrays", "./refineddataset/control", "./refineddataset/nodules.json", 0, transform)
 train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, collate_fn=collate_fn)
 
-val_dataset = NoduleDataset("./refineddataset/testxrays", "./refineddataset/control", "./refineddataset/nodules.json", 0.2, transform)
+val_dataset = NoduleDataset("./refineddataset/testxrays", "./refineddataset/control", "./refineddataset/nodules.json", 0, transform)
 val_loader = DataLoader(val_dataset, batch_size=1, shuffle=True, collate_fn=collate_fn)
 
 
@@ -62,20 +63,22 @@ params = [p for p in model.parameters() if p.requires_grad]
 
 optimizer = torch.optim.Adam(
     params,
-    lr=0.005,
+    lr=0.001,
     weight_decay=0.0005
 )
 
 # and a learning rate scheduler
 lr_scheduler = torch.optim.lr_scheduler.StepLR(
     optimizer,
-    step_size=3,
+    step_size=30,
     gamma=0.1
 )
 
 scaler = torch.amp.GradScaler()
 
 wandb.watch(model)
+
+metric = MeanAveragePrecision(iou_type="bbox")
 
 print("Started Training")
 
@@ -107,40 +110,46 @@ for epoch in range(NUM_EPOCHS):
     print(f"Evaluating for epoch: {epoch}")
 
     model.eval()
-
     
-    coco = get_coco_api_from_dataset(val_loader.dataset)
-    iou_types = _get_iou_types(model)
-    coco_evaluator = CocoEvaluator(coco, iou_types)
+    #metric = MeanAveragePrecision(iou_type="bbox")
+    
+    #coco = get_coco_api_from_dataset(val_loader.dataset)
+    #iou_types = _get_iou_types(model)
+    #coco_evaluator = CocoEvaluator(coco, iou_types)
 
     total_negative, correct_negative = 0, 0
 
     for images, targets in val_loader:
         images = list(image.to(device) for image in images)
-
-        outputs = model(images)
+        
+        with torch.no_grad():
+            outputs = model(images)
+        
         outputs = [{k: v.to(cpu_device) for k, v in t.items()} for t in outputs]
-
-        res = {target["image_id"]: output for target, output in zip(targets, outputs)}
-
+        
         if not is_negative_target(targets):
-            coco_evaluator.update(res)
+            metric.update(outputs, targets)
         else:
             total_negative += 1
             if is_negative_target(outputs): correct_negative += 1
-
-    #print(len(val_loader), total_negative, correct_negative)
     
-    coco_evaluator.accumulate()
-    coco_evaluator.summarize()
+    #coco_evaluator.accumulate()
+    #coco_evaluator.summarize()
 
+    if total_negative == 0:
+        control_accuracy = -1
+    else:
+        control_accuracy = correct_negative/total_negative
+    
+    results = metric.compute()
+    metric.reset()
 
     # first - iou=0.5:0.95, second - iou=0.50, third - iou=0.75
-    iou, iou50, iou75 = coco_evaluator.coco_eval["bbox"].stats[:3]
+    iou, iou50, iou75 = results["map"], results["map_50"], results["map_75"]
     avg_train_loss = sum(avg_train_loss)/len(avg_train_loss)
 
     # control accuracy is how good on control images, AP is for non-control (positive) images
-    wandb.log({"train loss - epoch": avg_train_loss, "eval AP iou=0.5:0.95 - epoch": iou, "eval AP iou=0.50 - epoch": iou50, "eval AP iou=0.75 - epoch": iou75, "control accuracy": correct_negative/total_negative})
+    wandb.log({"train loss - epoch": avg_train_loss, "eval AP iou=0.5:0.95 - epoch": iou, "eval AP iou=0.50 - epoch": iou50, "eval AP iou=0.75 - epoch": iou75, "control accuracy": control_accuracy})
 
     if epoch % SAVE_MODEL_INTERVAL == 0:
         torch.save(model, f"fcnn{epoch}.pth")
