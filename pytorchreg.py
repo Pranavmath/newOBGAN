568bb758f0341f140f382bbb2b1b39298f85c184
Pranav <pranav.sambhu@gmail.com>
2025-02-13 22:14:31 -0500

    fixed some stuff

--- pytorchreg.py
+++ pytorchreg.py
@@ -1,22 +1,12 @@
-from datasets import NoduleDataset, CurriculumNoduleDataset
-from torch.utils.data import DataLoader
-from tqdm import tqdm
-import os
-import random
 import json
-import torchvision
 from torchvision import transforms
-from torchvision.models.detection.faster_rcnn import FastRCNNPredictor, fasterrcnn_resnet50_fpn_v2 as fcnn
 import torch
-import wandb
-from torchmetrics.detection import MeanAveragePrecision, IntersectionOverUnion
-from torchvision.models.detection.rpn import AnchorGenerator
-import torchvision.models.detection._utils as det_utils
-import numpy as np 
-from scipy.interpolate import RBFInterpolator
+from torchmetrics.detection import IntersectionOverUnion
 import matplotlib.pyplot as plt 
 from matplotlib import cm
-
+from PIL import Image
+import os
+from tqdm import tqdm
 
 transform = transforms.Compose([
     transforms.Resize(1024),
@@ -27,41 +17,49 @@ transform = transforms.Compose([
 device = torch.device("cuda")
 cpu_device = torch.device("cpu")
 
+PATH = "fcnn108.pth"
+
 model = torch.load(PATH).to(device)
 
 annotations = json.load(open("./refineddataset/nodules.json"))
-metadata = json.load(open("./refineddataset/nodulemetada.json"))
+metadata = json.load(open("./refineddataset/nodulemetadata.json"))
 
 metric = IntersectionOverUnion()
 
 areas, brightnesses, ious = [], [], []
 
-for image, nodules in annotations.items():
-    image = Image.open(image + ".jpg")
-    image = transform(image)
+xray_path = "./refineddataset/testxrays"
+
+for image in tqdm(os.listdir(xray_path)):
+    nodules = annotations[image.split(".")[0]]
+    image = Image.open(os.path.join(xray_path, image))
+    image = transform(image).to(device)
 
     with torch.no_grad():
-        outputs = model(image)
+        outputs = model([image])
 
     outputs = [{k: v.to(cpu_device) for k, v in t.items()} for t in outputs]
 
     for nodule_name, nodule_bbox in nodules:
         _, area, brightness = metadata[nodule_name]
 
-        nodule_target = {
-            "boxes": torch.tensor(nodule_bbox, dtype=torch.float32),
+        nodule_target = [{
+            "boxes": torch.tensor([nodule_bbox], dtype=torch.float32),
             "labels": torch.ones(1, dtype=torch.int64),  # Assuming label for all boxes is 1 (nodule)
-        }
+        }]
 
-        iou = metrics(preds=outputs, target=targets)["iou"]
+        iou = metric(preds=outputs, target=nodule_target)["iou"].item()
 
-        areas.append(area)
-        brightnesses.append(brightness)
-        ious.append(iou)
+        if iou != 0:
+            areas.append(area)
+            brightnesses.append(brightness)
+            ious.append(iou)
 
 
 data = zip(areas, brightnesses)
 
-fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-ax.plot_surface(areas, brightnesses, iou, cmap=cm.Blues)
+#fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+#ax.scatter(areas, brightnesses, ious)
+
+plt.scatter(brightnesses, ious)
 plt.show()
