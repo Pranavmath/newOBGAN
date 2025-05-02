a731424651a0ab60ae5ace7a791e165c5e49fa30
Pranavmath <90156261+Pranavmath@users.noreply.github.com>
2025-05-01 23:51:03 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 apt-get install unzip vim -y
-git clone https://github.com/Pranavmath/newOBGAN.git
+git clone -b master https://github.com/Pranavmath/newOBGAN.git
 pip install wandb torchmetrics[detection]
 mv refineddataset.zip newOBGAN/refineddataset.zip
 unzip newOBGAN/refineddataset.zip
